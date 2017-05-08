f6f647369985dc9e5e77818992f8d75a9e37da3c
justengw <justengwatkins@gmail.com>
2017-05-08 09:32:28 -0700

    customized admim users

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register User do
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
@@ -11,5 +12,23 @@ ActiveAdmin.register User do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+
+	permit_params :email, :admin
+
+	form do |f|
+		f.semantic_errors
+		f.inputs do
+			f.input :email
+			f.input :admin
+		end
+		f.actions
+	end
+
+	index do
+	  id_column
+	  column :email
+	  column :admin
+	  actions
+	end
 
 end
